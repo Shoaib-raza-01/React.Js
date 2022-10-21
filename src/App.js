a0a03bda843061cdbf45adbbaea4b20eccc25827
shoaib raza <razashoaib270@gmail.com>
2022-10-21 20:04:42 +0530

    enabled DARK mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+  const [text, setText] = useState('Dark')
+  
+  const onToggle = ()=>{
+    if(mode=== 'light'){
+      setMode('dark')
+      setText('Light')
+      document.body.style.backgroundColor = "#202b36"
+    }else{
+      setMode('light')
+      setText('Dark')
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
     <>
-      <Navbar />
+      <Navbar theme = {mode} Mode = {text} toggleMode={onToggle} />
       <div className="container my-3">
-        <TextForm heading = "Enter your text here "/>
+        <TextForm heading = "Enter your text here" theme = {mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,24 +5,25 @@ export default function TextForm(props) {
   const [dark , myDark] = useState(
     {
       color : "white",
-      backgroundColor : "black"
+      backgroundColor : "#405368"
     }
   )
-  const [btn , setBtn] = useState("Enable Dark Mode")
+  const [btn , setBtn] = useState("Enable Light Mode")
 
   // useState is a hook,  Hooks let us use state and other React features without writing a class (uses function😅)
 
   const clickModeHandler = ()=>{
     if(dark.color === "white"){
       myDark({
-        color : "black",
+        color : "#405368",
         backgroundColor : "white"
       })
       setBtn("Enable Dark Mode")
+
     }else{
       myDark({
         color : "white",
-        backgroundColor : "black"
+        backgroundColor : "#405368"
       })
       setBtn("Enable Light Mode")
     }
@@ -39,6 +40,13 @@ export default function TextForm(props) {
     setText(newText)
 }
 
+const clickCopyHandler = ()=>{
+    var text = document.getElementById("txt")
+    // text.select()                                //selects the text
+    navigator.clipboard.writeText(text.value)
+}
+
+
   const handleOnChange = (event)=>{
     console.log("onchanged triggred")
     setText(event.target.value)
@@ -47,21 +55,22 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div>
+    <div style={{color : props.theme==='light'?'black':'white'}}>
         <h3>{props.heading}</h3>
         <div className="mb-3">
         <label htmlFor="txt" className="form-label">TextArea</label>
-        <textarea className="form-control" style={dark} value = {text} id="txt" onChange={handleOnChange} rows="8"></textarea>   
+        <textarea className="form-control" style={{backgroundColor :props.theme==='light'?'white':'#405368',color:props.theme==='light'?'black':'white'}} value = {text} id="txt" onChange={handleOnChange} rows="8"></textarea>   
          {/* we cannot edit the text body as the value is always set to text i.e. "enter ypur text here" ,
              to change the text we have to listen onchange that on a event in the function handleOnChange 
              and pass that event to setText(event.yarget.value) function*/} 
         </div>
         <button className="btn btn-primary" style={dark} onClick={clickUpHandler}>Uppercase</button>
         <button className="btn btn-outline-primary mx-2" style={dark} onClick={clickLoHandler}>Lowercase</button>
-        <button className="btn btn-outline-primary mx-2" style={dark} onClick={clickModeHandler}>{btn}</button>
+        <button className="btn btn-outline-primary" style={dark} onClick={clickModeHandler}>{btn}</button>
+        <button className="btn btn-outline-primary mx-2" style={dark} onClick={clickCopyHandler}>Copy</button>
 
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color : props.theme==='dark'?'white':'black'}}>
       <h2>Your Text Summary</h2>
       <p>
         {text.split(" ").length} words and {text.length} characters 
